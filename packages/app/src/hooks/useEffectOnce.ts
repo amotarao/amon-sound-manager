2c9750a1842b9fd7ac353dfe996c4e4e7cdce44e
Amon Sawamura <amotarao@gmail.com>
2022-09-22 23:19:56 +0200

    add useEffectOnce

---
+++ packages/app/src/hooks/useEffectOnce.ts
@@ -0,0 +1,6 @@
+import { useEffect } from 'react';
+
+export const useEffectOnce = (effect: React.EffectCallback) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(effect, []);
+};
